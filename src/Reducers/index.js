7091c2df607f16f5641d0428657309e9d8de6600
nabeelmumtaz92 <nabeelmumtaz92@gmail.com>
2024-05-26 20:48:57 -0500

    3rd commit

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,15 +1,9 @@
-// src/actions/index.js
-export const fetchNationality = (name) => {
-    return async (dispatch) => {
-      dispatch({ type: 'FETCH_NATIONALITY_REQUEST' });
-  
-      try {
-        const response = await fetch(`https://api.nationalize.io?name=${name}`);
-        const data = await response.json();
-        dispatch({ type: 'FETCH_NATIONALITY_SUCCESS', payload: data });
-      } catch (error) {
-        dispatch({ type: 'FETCH_NATIONALITY_FAILURE', payload: error.message });
-      }
-    };
-  };
-  
+// src/reducers/index.js
+import { combineReducers } from 'redux';
+import nationalityReducer from './nationalityReducer';
+
+const rootReducer = combineReducers({
+  nationality: nationalityReducer,
+});
+
+export default rootReducer;
